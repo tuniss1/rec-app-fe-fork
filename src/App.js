e69ab404c42da54c7354dc6e6200c2f7a4b10b63
tdkhoaEA <retdkhoa@gmail.com>
2022-10-28 21:28:44 +0700

    create landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import rec_logo from './img/logo.png'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="bg-[#dd6612] h-screen w-screen flex flex-col items-center ">
+      <div className="my-auto flex flex-col items-center ">
+        <img src={rec_logo} alt="logo" className=" sm:w-[400px] sm:h-[420px] w-[200px] h-[200px]" />
+        <p className="sm:text-3xl font-extrabold sm:mt-[32px] sm:w-[296px] mt-[24px] w-[312px] text-[28px] text-center"> WEBSITE IS UNDER DEVELOPMENT</p>
+        <p className="sm:text-sm font-normal sm:mt-[32px] sm:w-[360px] mt-[24px] w-[320px] text-[12px] text-center"> If you have cardboard boxes, aluminum cans or (unused) plastic bottles you want to sell, please contact us +84 8 2525 1567</p>
+        {/* <button
+          class="hover:shadow-form rounded-md bg-[#6A64F1] py-3 px-8 text-base font-semibold text-white outline-none"
         >
-          Learn React
-        </a>
-      </header>
+          Submit
+        </button> */}
+      </div>
     </div>
   );
 }
