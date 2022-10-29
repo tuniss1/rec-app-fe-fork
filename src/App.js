b6f963cf5f9829b55f6d60cdc0b32b568062bdd7
tdkhoaEA <retdkhoa@gmail.com>
2022-10-29 13:31:03 +0700

    change bg color

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="bg-[#dd6612] h-screen w-screen flex flex-col items-center ">
+    <div className="bg-[#ee5d00] h-screen w-screen flex flex-col items-center ">
       <div className="my-auto flex flex-col items-center ">
         <img src={rec_logo} alt="logo" className=" sm:w-[400px] sm:h-[420px] w-[62%] h-auto " />
         <p className="sm:text-3xl font-extrabold sm:mt-[32px] sm:w-[296px] mt-[24px] w-[312px] text-[28px] text-center text-white"> WEBSITE IS UNDER DEVELOPMENT</p>
