c9b1c76d41723f773b8877d60aed6541fc12922c
tdkhoaEA <retdkhoa@gmail.com>
2022-10-30 11:17:18 +0700

    fix styling

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="bg-[#ee5d00] h-screen w-screen flex flex-col items-center ">
       <div className="my-auto flex flex-col items-center ">
         <img src={rec_logo} alt="logo" className=" sm:w-[400px] sm:h-[420px] w-[62%] h-auto " />
-        <p className="sm:text-3xl font-extrabold sm:mt-[32px] sm:w-[296px] mt-[24px] w-[312px] text-[28px] text-center text-white leading-4"> WEBSITE IS UNDER DEVELOPMENT</p>
+        <p className="sm:text-3xl font-extrabold sm:mt-[32px] sm:w-[296px] mt-[24px] w-[312px] text-[28px] text-center text-white leading-[30px]"> WEBSITE IS UNDER DEVELOPMENT</p>
         <p className="sm:text-sm font-normal sm:mt-[32px] sm:w-[360px] mt-[24px] w-[264px] text-[12px] text-white text-center"> If you have cardboard boxes, aluminum cans or (unused) plastic bottles you want to sell, please contact us +84 825 251 567</p>
         {/* <p className="animate-bounce"> Contact us</p> */}
         {/* <button
