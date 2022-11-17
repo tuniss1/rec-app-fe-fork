9f0f18d020e542be0aa76a482a4e9f8585b4d1a4
khoatr1509 <retdkhoa@gmail.com>
2022-11-17 19:42:04 +0700

    update rec-app

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -10,7 +10,7 @@ function HomePage() {
                 <img src={rec_logo} alt="logo" className=" sm:w-[400px] sm:h-[420px] w-[62%] h-auto " />
                 <p className="sm:text-3xl font-extrabold sm:mt-[32px] sm:w-[296px] mt-[24px] w-[312px] text-[28px] text-center text-white leading-[30px]"> WEBSITE IS UNDER DEVELOPMENT</p>
                 <p className="font-normal sm:mt-[32px] sm:w-[360px] mt-[24px] w-[342px] text-[16px] text-white text-center"> If you have cardboard boxes, aluminum cans or (unused) plastic bottles you want to sell, please contact us +84 825 251 567</p>
-                <Link class=" hover:underline bg-white  text-gray-700 font-bold py-1 px-4 rounded mt-3" to="/">Month</Link>
+                {/* <Link class=" hover:underline bg-white  text-gray-700 font-bold py-1 px-4 rounded mt-3" to="/">Month</Link> */}
             </div>
         </div>
     );
